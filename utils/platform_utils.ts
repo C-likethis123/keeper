aea421eaa7342b3ea4113557169f35e4dfacdb7c
Chow Jia Ying <chowjiaying211@gmail.com>
2026-02-17 13:28:02 +0800

    Remove unused platform utils

--- utils/platform_utils.ts
+++
@@ -1,6 +0,0 @@
-export function getPathBasename(path: string): string {
-    if (!path) return '';
-    const parts = path.split(/[/\\]/).filter(Boolean);
-    return parts.length > 0 ? parts[parts.length - 1] : path;
-  }
-  
