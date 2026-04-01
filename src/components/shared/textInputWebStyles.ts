0f0cd438b14138fd4e2a6a6f1113f4139603585d
Chow Jia Ying <chowjiaying211@gmail.com>
2026-04-02 00:38:32 +0800

    More simplify

--- src/components/shared/textInputWebStyles.ts
+++ src/components/shared/textInputWebStyles.ts
@@ -1,18 +1,8 @@
 import { Platform, type TextStyle } from "react-native";
 
-export const webTextInputReset: TextStyle =
-	Platform.OS === "web"
-		? {
-				outlineStyle: "none",
-				outlineWidth: 0,
-				boxShadow: "none",
-			}
-		: {};
-
 export const webMultilineTextInputReset: TextStyle =
 	Platform.OS === "web"
 		? {
-				...webTextInputReset,
 				overflow: "hidden",
 			}
 		: {};
