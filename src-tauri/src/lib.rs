a7dc49cfeab7c7a5745291f81cbba19976cd89d3
Chow Jia Ying <chowjiaying211@gmail.com>
2026-04-08 00:41:37 +0800

    Fix PDF picker?

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -101,6 +101,7 @@ fn git_changed_markdown_paths_repo(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
